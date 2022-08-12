efd28af09f7e36645a63025fa1af5450f9ef12cc
Igor_U <fixtor75@gmail.com>
2022-08-12 22:40:22 +0300

    Начало дз в процессе

---
+++ Exe1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число больше 10:");
+int number = Convert.ToInt32(Console.ReadLine());
+int fistDigit = 0;
+int lastDigit = 0;
+if (number < 10)
+    Console.WriteLine($"Вы ввели неверное число {number} < 10");
+
+else
+{
+    while (number != fistDigit)
+        lastDigit = number % 10;
+    if (lastDigit != fistDigit)
+        Console.WriteLine($"Число {number} не палиндром");
+    else
+        Console.WriteLine($"Число {number} палиндром");
+}
