e6b852db71add747dfb7cdec77ee4ace25ae0261
Igor_U <fixtor75@gmail.com>
2022-08-13 23:18:03 +0300

    Задание 19 готво

--- Exe1v2/Program.cs
+++ Exe1v2/Program.cs
@@ -1,12 +1,4 @@
-﻿int numberLenght(int num) // Проверка количества символов в числах
-{
-    int i = 0;
-    for (i = 0; num > 0; i++)
-        num = num / 10;
-    return i;
-}
-
-int getBackDigit(int num) // Игра переверни число
+﻿int getBackDigit(int num) // Игра переверни число
 {
     int counter = 0;
     while (num > 0)
@@ -19,16 +11,9 @@ int getBackDigit(int num) // Игра переверни число
 
 Console.WriteLine("Введите 1-е число больше 10:");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите 2-е число больше 10:");
-int number2 = Convert.ToInt32(Console.ReadLine());
-
-if (numberLenght(number) == numberLenght(number2)) //Проверяем совпадают ли по длине числа
 {
-    if (getBackDigit(number) == number2) // Cравниваем перевернутое число с заданным палиндром
-        Console.WriteLine($"Введеное вами 1 число {number} Является палиндромом {number2}");
+    if (getBackDigit(number) == number) // Cравниваем перевернутое число с заданным палиндромом
+        Console.WriteLine($"Введеное вами 1 число {number} Является палиндромом {getBackDigit(number)}");
     else
-        Console.WriteLine($"Введеное вами 1 число {number} Не является палиндромом {number2}!!!");
+        Console.WriteLine($"Введеное вами 1 число {number} Не является палиндромом {getBackDigit(number)}!!!");
 }
-else
-    Console.WriteLine($"Введеное вами 1 число {number} отлично по длине от {number2}");
-    
