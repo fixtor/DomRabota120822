eaf3430879c7c41c70819e6908816db323dc291f
Igor_U <fixtor75@gmail.com>
2022-08-13 21:06:23 +0300

    Задание 19 готово

--- Exe1v2/Program.cs
+++ Exe1v2/Program.cs
@@ -6,7 +6,7 @@
     return i;
 }
 
-int getBackDigit(int number)
+int getBackDigit(int number) // Игра переверни число
 {
     int counter = 0;
     while (number > 0)
@@ -19,15 +19,14 @@ int getBackDigit(int number)
 
 Console.WriteLine("Введите 1-е число больше 10:");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Перевертыш {getBackDigit(number)}");
 Console.WriteLine("Введите 2-е число больше 10:");
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (numberLenght(number)==numberLenght(number2))
 {
     if (getBackDigit(number) == number2)
-    Console.Writeline($"Введеное вами 1-е число {number} Является палиндромом числа 2 {number2}");
+    Console.WriteLine($"Введеное вами 1 число {number} Является палиндромом {number2}");
     else
-    Console.Writeline($"Введеное вами 1-е число {number} Не является палиндромом числа 2 {number2}");
+    Console.WriteLine($"Введеное вами 1 число {number} Не является палиндромом {number2}!!!");
 }
 else 
-Console.Writeline($"Введеное вами 1-е число {number} отлично по длине от {number2}");
+Console.WriteLine($"Введеное вами 1 число {number} отлично по длине от {number2}");
