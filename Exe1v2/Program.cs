d89c1aee4e9c08a37cf3e214843aca456ad11931
Igor_U <fixtor75@gmail.com>
2022-08-13 23:22:12 +0300

    Задание 19 исправлено в соответствии с условием

--- Exe1v2/Program.cs
+++ Exe1v2/Program.cs
@@ -9,11 +9,11 @@
     return counter;
 }
 
-Console.WriteLine("Введите 1-е число больше 10:");
+Console.WriteLine("Введите число больше 10:");
 int number = Convert.ToInt32(Console.ReadLine());
 {
     if (getBackDigit(number) == number) // Cравниваем перевернутое число с заданным палиндромом
-        Console.WriteLine($"Введеное вами 1 число {number} Является палиндромом {getBackDigit(number)}");
+        Console.WriteLine($"Введеное вами число {number} Является палиндромом {getBackDigit(number)}");
     else
-        Console.WriteLine($"Введеное вами 1 число {number} Не является палиндромом {getBackDigit(number)}!!!");
+        Console.WriteLine($"Введеное вами число {number} Не является палиндромом!!!");
 }
