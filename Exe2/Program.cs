e15d7d4705f9f85f467735fea8abb77874aea802
Igor_U <fixtor75@gmail.com>
2022-08-13 22:23:50 +0300

    Задача 23 готова

---
+++ Exe2/Program.cs
@@ -0,0 +1,15 @@
+﻿void Digits(int num)
+{
+    int i = 0;
+    int count = 0;
+    for (i = 1; i <= num; i++)
+    {
+        count = Convert.ToInt32(Math.Pow(i, 3));
+        Console.WriteLine($"{i} ^ 3 = {count}");
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+Digits(number);
