098b8fce1c5b29ff9a3ca6edc02490c27d96cb24
Igor_U <fixtor75@gmail.com>
2022-08-13 23:19:28 +0300

    Решение на сравнение двух зеркальных чисел

---
+++ Exe1 forTwoNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿int numberLenght(int num) // Проверка количества символов в числах
+{
+    int i = 0;
+    for (i = 0; num > 0; i++)
+        num = num / 10;
+    return i;
+}
+
+int getBackDigit(int num) // Игра переверни число
+{
+    int counter = 0;
+    while (num > 0)
+    {
+        counter = counter * 10 + num % 10;
+        num /= 10;
+    }
+    return counter;
+}
+
+Console.WriteLine("Введите 1-е число больше 10:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число больше 10:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (numberLenght(number) == numberLenght(number2)) //Проверяем совпадают ли по длине числа
+{
+    if (getBackDigit(number) == number2) // Cравниваем перевернутое число с заданным палиндром
+        Console.WriteLine($"Введеное вами 1 число {number} Является палиндромом {number2}");
+    else
+        Console.WriteLine($"Введеное вами 1 число {number} Не является палиндромом {number2}!!!");
+}
+else
+    Console.WriteLine($"Введеное вами 1 число {number} отлично по длине от {number2}");
